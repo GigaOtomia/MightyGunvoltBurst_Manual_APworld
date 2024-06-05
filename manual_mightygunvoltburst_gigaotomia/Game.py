dae25adc1d849c1ea4102fa893d2414b46e6299c
Giga Otomia <32317465+GigaOtomia@users.noreply.github.com>
2024-06-05 11:52:14 -0400

    Update Game.py
    fix my fucky wucky

--- manual_mightygunvoltburst_gigaotomia/Game.py
+++ manual_mightygunvoltburst_gigaotomia/Game.py
@@ -3,7 +3,7 @@ from .Data import game_table
 if 'creator' in game_table:
     game_table['player'] = game_table['creator']
 
-game_name = "Mighty Gunvolt Burst Gigmanual"
+game_name = "Manual_%s_%s" % (game_table["game"], game_table["player"])
 filler_item_name = game_table["filler_item_name"] if "filler_item_name" in game_table else "Filler"
 starting_items = game_table["starting_items"] if "starting_items" in game_table else None
 
